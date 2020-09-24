a89b0837390e0fdae99793dc8e39f75dc6a14b1c
bbrow <bbrower@smu.edu>
2020-09-24 15:06:10 -0500

    added recursive solution

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+
+//recursion
+function fib1(n){
+    //base case
+    if (n < 3){
+        return n - 1;
+    } else {
+        return recursive(n - 1) + recursive (n - 2)
+    }
+    
+}
+
 //iterative - slowest solution b/c of recursion
-function iterative(n) {
+function fib0(n) {
     let arr = [0, 1];
     for (let i = 2; i < n; i++){
         arr.push(arr[i - 1] + arr[i - 2]);
