3b9aa49c788201429b2bcb0e925f2265a355c12c
bbrow <bbrower@smu.edu>
2020-09-24 15:04:46 -0500

    iterative solution with for loop

---
+++ index.js
@@ -0,0 +1,9 @@
+//iterative - slowest solution b/c of recursion
+function iterative(n) {
+    let arr = [0, 1];
+    for (let i = 2; i < n; i++){
+        arr.push(arr[i - 1] + arr[i - 2]);
+        console.log(arr)
+    }
+    return arr[n-1]
+}
